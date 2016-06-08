70c1faa4a6e01c7b59cac163c446344230375ca0
lxx <listen987654321@sina.com>
2016-06-08 16:56:17 +0800

    Initial Commit

---
+++ HaveFriends/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HaveFriends
+//
+//  Created by lxx on 16/6/8.
+//  Copyright © 2016年 lxx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
